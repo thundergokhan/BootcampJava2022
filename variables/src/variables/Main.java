f351a3a11d14ba0cbf0fb855440eea931c65cafc
Gökhan Yıldırım <gokhan.y811@gmail.com>
2022-10-01 19:32:19 +0300

    variables

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class Main {
+	 		
+	//camel Casing = ogrenciSayisi
+	public static void main(String[] args) {
+		//case sensitive
+		int ogrenciSayisi = 12; 
+		String mesaj ="Öğrenci sayısı:";
+		System.out.println(mesaj  + ogrenciSayisi);
+		System.out.println(mesaj  + ogrenciSayisi);
+		System.out.println("Öğrenci Sayım : "+ ogrenciSayisi);
+		System.out.println("Öğrenci Sayım : "+ ogrenciSayisi);
+		System.out.println("Öğrenci Sayım : "+ ogrenciSayisi);
+	}
+
+}
