cd11cd9618c5f5f6386008bcdb5898a54664a184
Gökhan Yıldırım <gokhan.y811@gmail.com>
2022-10-01 16:56:41 +0300

    javaHelloWorld

---
+++ javaHelloWorld/src/javaHelloWorld/Main.java
@@ -0,0 +1,11 @@
+package javaHelloWorld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//intellisense
+		System.out.println("Merhaba Java");
+		System.out.println("Merhaba Java 2");
+	}
+
+}
