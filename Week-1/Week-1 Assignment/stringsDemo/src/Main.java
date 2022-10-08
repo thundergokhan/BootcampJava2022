7185d56d056a5e3bd1e16efea6afe7dadd0f7bad
Gökhan Yıldırım <82091624+thundergokhan@users.noreply.github.com>
2022-10-08 20:31:40 +0300

    Update Main.java

--- Week-1/Week-1 Assignment/stringsDemo/src/Main.java
+++ Week-1/Week-1 Assignment/stringsDemo/src/Main.java
@@ -1,35 +1,23 @@
+package stringsDemo;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		String mesaj = "Bugün hava çok güzel.";
+		System.out.println(mesaj);
 
-	    String message = "Bugün hava çok güzel. ";
-        System.out.println(message);
+		System.out.println("Eleman sayısı:" + mesaj.length());
+		System.out.println("5. eleman:" + mesaj.charAt(4)); // 0'dan başladığı için 5. eleman 4. indexteki elemandır
+		System.out.println(mesaj.concat(" Yaşasın!"));
+		System.out.println(mesaj.startsWith("A")); // B ile başlıyor mu diye sorar başlıyorsa ture döndürür eğer
+													// başlamıyorsa false döndürür. (Büyük-küçük harfe dikkat)
+		System.out.println(mesaj.endsWith(".")); // nokta ile bitiyorsa true
+		char[] karakterler = new char[5];
+		mesaj.getChars(0, 4, karakterler, 0);
+		System.out.println(karakterler);
+		System.out.println(mesaj.indexOf("av")); // indexof karakterin mesajın içinde kaçıncı eleman
+												// olduğunu bulmaya yarar
+		System.out.println(mesaj.lastIndexOf("a")); // aramaya sağdan başlar
+	}
 
-        System.out.println("Eleman sayısı : " + message.length());
-        System.out.println("5.eleman : "+ message.charAt(4));
-        System.out.println(message.concat(" Yaşasın!"));
-        System.out.println(message.startsWith("b")); //false-case sensitive
-        System.out.println(message.endsWith("."));
-
-        char[] characters = new char[5];
-        message.getChars(0,5,characters,0);
-        System.out.println(characters);
-
-        System.out.println(message.indexOf("av"));
-        System.out.println(message.lastIndexOf("a"));
-
-        String newMessage = message.replace(' ', '-');
-        System.out.println(newMessage);
-
-        System.out.println(message.substring(2,5));
-
-        for(String word : message.split(" ")){
-            System.out.println(word);
-        }
-
-        System.out.println(message.toLowerCase());
-        System.out.println(message.toUpperCase());
-        System.out.println(message.trim());
-    }
 }
