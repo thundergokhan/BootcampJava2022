b5ad2c96dc8d1f9483a812c80cf21b9226cd90b7
Gökhan Yıldırım <82091624+thundergokhan@users.noreply.github.com>
2022-10-08 20:32:36 +0300

    Update Main.java

--- Week-1/Week-1 Assignment/switchDemo/src/Main.java
+++ Week-1/Week-1 Assignment/switchDemo/src/Main.java
@@ -1,26 +1,31 @@
+package switchDemo;
+
 public class Main {
+	// char verdiğimiz için tek tırnak
+	public static void main(String[] args) {
+		char grade = 'C';
 
-    public static void main(String[] args) {
-	    char grade = 'F';
+		switch (grade) {
+		case 'A':
+			System.out.println("Mükemmel :Geçtiniz");
+			break;
+		// break bitirmek için kullanılır
+		case 'B':
+		case 'C':
+			System.out.println("İyi :Geçtiniz");
+			break;
+		case 'D':
+			System.out.println("Fena Değil : Geçtiniz");
+			break;
+		case 'F':
+			System.out.println("Maalesef Kaldınız");
+			break;
+		default:
+			System.out.println("Geçersiz not girdiniz");
 
-        switch(grade){
-            case 'A':
-                System.out.println("Mükemmel, geçtiniz");
-                break;
-            case 'B':
-                System.out.println("Çok güzel, geçtiniz");
-                break;
-            case 'C':
-                System.out.println("İyi, geçtiniz");
-                break;
-            case 'D':
-                System.out.println("Fena değil, geçtiniz");
-                break;
-            case 'F':
-                System.out.println("Maalesef kaldınız.");
-                break;
-            default:
-                System.out.println("Geçersiz not girdiniz.");
-        }
-    }
+		}
+		// Format Document or press Ctrl+Shift+F.
+
+	}
+
 }
